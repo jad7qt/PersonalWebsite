f5bb94692a0ee4e7cbb88653afade6064bc26da7
Jared <91357852+jad7qt@users.noreply.github.com>
2023-06-23 09:34:03 -0400

    Add linkedin Badge code

--- src/app/(marketing)/about/page.js
+++ src/app/(marketing)/about/page.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
+    
     <main>
+      <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
       <div>
+      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="jaredduttlink" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/jaredduttlink?trk=profile-badge">Jared Dutt</a></div>
+              
 
       </div>
     </main>
