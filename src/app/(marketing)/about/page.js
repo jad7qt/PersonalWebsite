00d411df80c86312edadd17467d2abbe37a8c71f
Jared <91357852+jad7qt@users.noreply.github.com>
2023-06-27 11:54:25 -0400

    Update page.js

--- src/app/(marketing)/about/page.js
+++ src/app/(marketing)/about/page.js
@@ -5,7 +5,8 @@ export default function Home() {
       <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
       <div>
       <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="jaredduttlink" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/jaredduttlink?trk=profile-badge">Jared Dutt</a></div>
-              
+              {/* use background-image: url( '/path/image.svg' ); 
+              in the CSS so that it doesn't have to be hard coded in the HTML */}
 
       </div>
     </main>
